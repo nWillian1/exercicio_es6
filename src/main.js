ea065819e5701e206ad8f8d80804ea4279040028
Willian Nogueira <willian.nogueira2016@hotmail.com>
2026-03-24 18:53:09 -0300

    Exercicio de arrays

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// 1º Passo: Criar um Array de objetos com atributos nome e notas de alunos
+
+const alunos = [
+        {nome: "João", nota: 10},
+        {nome: "Maria", nota: 8},
+        {nome: "Pedro", nota: 7},
+        {nome: "Ana", nota: 9},
+        {nome: "Lucas", nota: 6},
+        {nome: "Julia", nota: 5},
+        {nome: "Rafael", nota: 4},
+        {nome: "Isabela", nota: 3},
+]
+
+// 2º Passo: Criar uma função que irá retornar alunos com nota maior ou igual a 6.
+
+const alunosAprovados = alunos.filter(aluno => aluno.nota >= 6);
+
+console.log(alunosAprovados);
